c632171020928ca1980c49b5638248b232f1f451
telenths@gmail.com <telenths@gmail.com@b9ed21dc-1f62-25a4-e15a-dd15760db8d6>
2011-10-19 09:30:00 +0000

    add resources

---
+++ src/main/java/com/junshi/site/util/PageGen.java
@@ -0,0 +1,71 @@
+package com.junshi.site.util;
+
+import java.io.File;
+import java.io.FileFilter;
+import java.io.IOException;
+
+import com.junshi.util.FileUtil;
+
+public class PageGen {
+
+    private final String lineBreak = System.getProperty("line.separator");
+
+    private final String headReplacement = "<!--Head_Import_Start-->" + lineBreak
+                           + " <script type='text/javascript' src='js/googletrack.js'></script>      " + lineBreak
+                           + " <script type='text/javascript' src='js/jquery-1.6.4.min.js'></script> " + lineBreak
+                           + " <script type='text/javascript' src='js/page_init.js'></script>         " + lineBreak
+                           + " <link type='text/css' rel='stylesheet' href='css/main.css' />         " + lineBreak
+                           + "<!--Head_Import_End-->";
+
+
+    public static void main(String[] args) throws IOException {
+
+        PageGen p = new PageGen();
+        p.operate();
+
+    }
+
+    public void operate() throws IOException{
+        File root = new File("./src/main/webapp/junshi/html/");
+        operateFolder(root);
+    }
+
+    public void operateFolder(File file) throws IOException {
+
+        File[] fileList = file.listFiles(new FileFilter() {
+            @Override
+            public boolean accept(File pathname) {
+                return pathname.isDirectory() || (pathname.isFile() && pathname.getName().endsWith(".html"));
+            }
+        });
+
+        if(fileList == null )
+            return;
+
+        for (File f : fileList) {
+            if (f.isFile()) {
+                refineFile(f);
+            } else {
+                operateFolder(f);
+            }
+        }
+    }
+
+    public void refineFile(File file) throws IOException{
+        System.out.println(" ---- refining " + file.getAbsolutePath());
+
+        if(!file.getName().endsWith(".html"))
+            return;
+
+        String str = FileUtil.readFile(file);
+        str = replaceHead(str);
+        FileUtil.writeFile(file, str);
+    }
+
+    public String replaceHead(String str){
+        return str.replaceAll("<!--Head_Import_Start-->[.\\s\\S]*<!--Head_Import_End-->", headReplacement);
+    }
+
+
+
+}
