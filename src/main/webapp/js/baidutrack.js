11a3ee54416406da7b6158be02d85e7d8e4e7c53
telenths@gmail.com <telenths@gmail.com@b9ed21dc-1f62-25a4-e15a-dd15760db8d6>
2011-10-26 09:31:26 +0000

    baidu track

---
+++ src/main/webapp/js/baidutrack.js
@@ -0,0 +1,2 @@
+var _bdhmProtocol = (("https:" == document.location.protocol) ? " https://" : " http://");
+document.write(unescape("%3Cscript src='" + _bdhmProtocol + "hm.baidu.com/h.js%3Fd3f6e2ae8286f74281ac45c9e9cf14ec' type='text/javascript'%3E%3C/script%3E"));
