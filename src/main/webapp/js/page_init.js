d5b3dc740a49e15a6eff2f9e98b3206078e177db
telenths@gmail.com <telenths@gmail.com@b9ed21dc-1f62-25a4-e15a-dd15760db8d6>
2011-10-31 14:48:00 +0000

    solution menu as replacement

--- src/main/webapp/js/page_init.js
+++ src/main/webapp/js/page_init.js
@@ -10,40 +10,13 @@ function initFooterLinks() {
     });
 }
 
-function initMenuSolution(){
-    $.getJSON('/json/menu_solutions.json?t=$[DATETIME]', function(data) {
-        var html = "";
-        $.each(data, function(entryIndex, entry) {
-            if (entry['menu']) {
-                html += "<a style='width:100%' href='" + entry['link'] + "'><div class='"+entry['class']+"'>" + entry['menu'] + "</div></a>";
-            }
-            
-            if(entry['data']){
-            	$.each(entry['data'], function(entryIndex, entry){
-                  if (window.location.pathname == entry['link'])
-                	  html += "<div class='"+entry['class']+"_current'>" + entry['item'] + "</div>";
-                  else
-                	  html += "<a style='width:100%' href='" + entry['link'] + "'><div class='"+entry['class']+"'>" + entry['item'] + "</div></a>";
-            	});
-            }
-            
-        });
-        $('#menu_solution').prepend(html);
-    });
-}
-
 $(document).ready(function() {
 	initFooterLinks();
-    if($("#menu_solution")){
-    	initMenuSolution();
-    }
-    
 
 	$('#table_contact tr>td:even').addClass('center_align_left');
 	$('#table_contact tr>td:odd').addClass('center_align_right');
 
 	$('.salse_section tr>td:even').addClass('center_align_left');
 	$('.salse_section tr>td:odd').addClass('center_align_right');
-	$('.salse_section tr:first-child>td:even').removeClass('center_align_left');
     
 });
