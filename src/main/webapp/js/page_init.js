f32de1993fc04f68bd7a769b492a83492beac31d
telenths@gmail.com <telenths@gmail.com@b9ed21dc-1f62-25a4-e15a-dd15760db8d6>
2011-11-02 14:46:51 +0000

    add content footer

--- src/main/webapp/js/page_init.js
+++ src/main/webapp/js/page_init.js
@@ -10,8 +10,16 @@ function initFooterLinks() {
     });
 }
 
+function initBodyFooter(){
+	
+	html = "<tr><td class='left_menu'></td><td class='page_footer'>最新价格及详细内容欢迎咨询。电话：13764193800 邮件：bkcailiang@163.com</td></tr>";
+	
+	$("#table_body").append(html);
+}
+
 $(document).ready(function() {
 	initFooterLinks();
+	initBodyFooter();
 
 	$('#table_contact tr>td:even').addClass('center_align_left');
 	$('#table_contact tr>td:odd').addClass('center_align_right');
