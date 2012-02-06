e32480b9ab1e2a8d3098e68334e8034e23313dcf
telenths@gmail.com <telenths@gmail.com@b9ed21dc-1f62-25a4-e15a-dd15760db8d6>
2012-02-06 09:00:06 +0000

    track download actions

--- trunk/src/main/webapp/js/googletrack.js
+++ trunk/src/main/webapp/js/googletrack.js
@@ -3,6 +3,15 @@
   _gaq.push(['_setAccount', 'UA-19461614-5']);
   _gaq.push(['_trackPageview']);
 
+  _gaq.push(
+    ['_setAccount','UA-19461614-5'],
+    ['_addOrganic','soso','w'],
+    ['_addOrganic','sogou','query'],
+    ['_addOrganic','yodao','q'],
+    ['_addOrganic','bing','q'],
+    ['_addOrganic','gougou','search']
+  );
+
   (function() {
     var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
     ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
